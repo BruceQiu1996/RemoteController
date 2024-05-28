43da60ea277f250e2b2a16d3cdab6965c454bf1f
qiuwei <bruce.qiu@meetfuture.com>
2024-05-28 13:30:07 +0800

    'Initializeproject'

---
+++ RemoteController.Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace RemoteController.Server
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Host.ConfigureServices((hostContext, services) =>
+            {
+                services.AddControllers();
+                services.AddEndpointsApiExplorer();
+            }).UseSerilog((context, logger) =>
+            {
+                logger.WriteTo.Console();
+            });
+            builder.Services.AddControllers();
+            builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            var app = builder.Build();
+            app.Urls.Add("http://*:5000");
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(option =>
+            {
+                option.SwaggerEndpoint($"/swagger/v1/swagger.json", "v1");
+            });
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
